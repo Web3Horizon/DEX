6eb42def915b73cc268ef6f3aa162092ca40ab21
RepoRover <web.yugaitrov@gmail.com>
2024-11-11 21:02:25 +0200

    Added tailwindcss packages

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	// Applies styles to the whole app
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
+<!-- Render all the children, i.e. +page.svelte -->
 {@render children()}
+
+<style lang="postcss">
+</style>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-center text-4xl font-bold">Hello world!</h1>
