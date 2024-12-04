7816a9e722035f70f0af10ac8e1ed92c0a867eed
Hamidreza Yourdkhani <hamidreza.yourdkhani1993@gmail.com>
2024-12-04 20:55:55 +0200

    deleted the unnecessary files

--- src/routes/liquidity/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    // Add any necessary logic here
-  </script>
-  
-  <h1>Add Liquidity</h1>
-  <p>Welcome to the Add Liquidity page!</p>
-  
--- src/routes/liquidity/your-liquidity/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    // Add any necessary logic here
-  </script>
-  
-  <h1>Your Liquidity</h1>
-  <p>Here is a list of your liquidity pools.</p>
-  
